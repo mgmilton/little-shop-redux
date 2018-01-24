3dfe674b3f672b062920190ea3ba01ff364979e2
mgmilton <matt.milton@gmail.com>
2018-01-24 09:34:20 -0700

    adds validation test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,10 +1,16 @@
 RSpec.describe Merchant do
 
   describe "Validations" do
-    context
+    context 'data validations'
     it "merchant entry is invalid without a name" do
       merchant = Merchant.create()
       expect(merchant).to_not be_valid
+    end
+
+    it 'merchant entry is invalid if name is not unique' do
+      merchant = Merchant.create(name: "Deadbeat")
+      merchant_2 = Merchant.create(name: "Deadbeat")
+      expect(merchant_2).to_not be_valid
     end
   end
 end
