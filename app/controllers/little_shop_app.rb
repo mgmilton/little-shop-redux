cffaa77b053b7900a9e1992c3f8051c5d291ef7a
mgmilton <matt.milton@gmail.com>
2018-01-24 16:19:15 -0700

    through it1

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,6 +1,47 @@
+require_relative "../models/merchant.rb"
+
 class LittleShopApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
+
   get '/merchants' do
     @merchants = Merchant.all
     erb :"merchants/index"
   end
+
+  get '/merchants/new' do
+    erb :"merchants/new"
+  end
+
+  get '/merchants/:id' do
+    @merchant = Merchant.find_by(id: params[:id])
+    erb :"merchants/show"
+  end
+
+  get '/merchants/:id/edit' do
+    @merchant = Merchant.find_by(id: params[:id])
+    erb :"merchants/edit"
+  end
+
+  post '/merchants' do
+    Merchant.create(params[:merchant])
+    redirect '/merchants'
+  end
+
+  put '/merchants/:id' do
+    merchant = Merchant.find_by(id: params[:id])
+    merchant.update(name: params[:merchant].values.first)
+    redirect "/merchants/#{merchant.id}"
+  end
+
+  delete '/merchants/:id' do
+    merchant = Merchant.find_by(id: params[:id])
+    merchant.destroy
+    redirect '/merchants'
+  end
+
 end
