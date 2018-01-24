0a7940879bae21e381f0d17a256f7c978aeebc7e
mgmilton <matt.milton@gmail.com>
2018-01-23 20:55:55 -0700

    adds rspec test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,4 @@
 class Merchant < ActiveRecord::Base
-  validates_presence_of :name
+  validates :name, presence: true, uniqueness: true
 
 end
--- db/migrate/20180124025051_create_merchants.rb
+++ db/migrate/20180124025051_create_merchants.rb
@@ -2,7 +2,6 @@ class CreateMerchants < ActiveRecord::Migration[5.1]
   def change
     create_table :merchants do |m|
       m.text       :name
-
       m.timestamps null: false
     end
   end
