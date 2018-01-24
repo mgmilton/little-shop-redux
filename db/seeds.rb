f6a21a511853613cc78b504455e94b5b19f490ca
mgmilton <matt.milton@gmail.com>
2018-01-24 16:34:15 -0700

    alters db:seed file

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require 'csv'
+require_relative '../app/models/merchant.rb'
+
+merchants = CSV.open("data/merchants.csv", headers: true, header_converters: :symbol)
+
+merchants.each do |row|
+  Merchant.create(id:         row[:id],
+                  name:       row[:id],
+                  created_at: row[:created_at],
+                  updated_at: row[:updated_at])
+end
