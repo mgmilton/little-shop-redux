d9c9c17873ca0c27d8197e145fdc89fbd2e889a3
mgmilton <matt.milton@gmail.com>
2018-01-23 19:57:04 -0700

    adds first migration

---
+++ db/migrate/20180124025051_create_merchants.rb
@@ -0,0 +1,9 @@
+class CreateMerchants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :merchants do |m|
+      m.text       :name
+
+      m.timestamps null: false
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,13 @@ ENV["RACK_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
+require 'pry'
+require 'bundler'
+Bundler.require(:default, :test)
 
 Capybara.app = LittleShopApp
+
+DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |c|
   c.include Capybara::DSL
