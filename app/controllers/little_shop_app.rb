6028d76a22b03066b91216e6ea8eec4452be956f
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-24 18:58:12 -0700

    commit to merge matt work

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,6 +1,30 @@
 class LittleShopApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :"merchants/index"
+  end
+
   get '/merchants' do
     @merchants = Merchant.all
     erb :"merchants/index"
+  end
+
+  post '/merchants/' do
+    Merchant.create()
+    erb :"merchants/index"
+
+    redirect '/merchants/index'
+  end
+
+  put 'merchant/update' do
+    Merchant.update(id.to_i, params[:manager])
+  end
+
+  delete '/merchants/:id' do |id|
+    Merchant.destroy(id.to_i)
+
+    redirect '/merchants/index'
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,5 +2,6 @@ require 'pg'
 
 class Merchant < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  
 
 end
