d20a9c55f83073e87011670f0606a42a6cac7df8
mgmilton <matt.milton@gmail.com>
2018-01-24 19:41:50 -0700

    works throuhg it4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 require 'pry'
 require 'csv'
 require_relative '../app/models/merchant.rb'
+require_relative '../app/models/category.rb'
 
 merchants = CSV.open("data/merchants.csv", headers: true, header_converters: :symbol)
 
 merchants.each do |row|
   Merchant.create(id:         row[:id],
-                  name:       row[:id],
+                  name:       row[:name],
+                  created_at: row[:created_at],
+                  updated_at: row[:updated_at])
+end
+
+categories = CSV.open("data/categories.csv", headers: true, header_converters: :symbol)
+
+categories.each do |row|
+  Category.create(id:         row[:id],
+                  name:       row[:name],
                   created_at: row[:created_at],
                   updated_at: row[:updated_at])
 end
