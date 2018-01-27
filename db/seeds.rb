396badea3fc4e4a307615ce2a65e38dc770c50d3
mgmilton <matt.milton@gmail.com>
2018-01-27 14:46:14 -0700

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,8 @@ require_relative '../app/models/category.rb'
 require_relative '../app/models/item.rb'
 
 merchants = CSV.open("data/merchants.csv", headers: true, header_converters: :symbol)
-
-merchants.each do |row|
-  Merchant.create(id:         row[:id],
-                  name:       row[:name],
-                  created_at: row[:created_at],
-                  updated_at: row[:updated_at])
-end
-
 categories = CSV.open("data/categories.csv", headers: true, header_converters: :symbol)
+items = CSV.open("data/items.csv", headers: true, header_converters: :symbol)
 
 categories.each do |row|
   Category.create(id:         row[:id],
@@ -22,13 +15,25 @@ categories.each do |row|
                   updated_at: row[:updated_at])
 end
 
-items = CSV.open("data/items.csv", headers: true, header_converters: :symbol)
+
+merchants.each do |row|
+  Merchant.create(id:         row[:id],
+                  name:       row[:name],
+                  created_at: row[:created_at],
+                  updated_at: row[:updated_at])
+end
+
+
+category_id_cycle = Category.pluck(:id).cycle
+
 
 items.each do |row|
   Item.create(id:          row[:id],
               name:        row[:name],
               description: row[:description],
               price:       row[:unit_price],
+              merchant_id: row[:merchant_id],
+              category_id: category_id_cycle.next ,
               image:       'data/images/default_item_img.jpg',
               created_at:  row[:created_at],
               updated_at:  row[:updated_at])
