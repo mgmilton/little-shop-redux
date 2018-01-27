cc46f4f5b48e8055d4f14eebbd8e698533f37442
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-26 18:05:29 -0700

    added divs and html skeleton to items/new page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 ENV["RACK_ENV"] ||= "test"
-
 require 'bundler'
 Bundler.require(:default, :test)
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
-require 'capybara/dsl'
+require 'capybara/rspec'
 
 Capybara.app = LittleShopApp
 
