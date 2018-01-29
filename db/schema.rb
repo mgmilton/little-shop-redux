fd03eff7bc684c9741634e3de61d7212823f6177
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-29 11:59:02 -0700

    add migration to document schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128233910) do
+ActiveRecord::Schema.define(version: 20180129185324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180128233910) do
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
     t.integer "item_count", default: 0
   end
 
