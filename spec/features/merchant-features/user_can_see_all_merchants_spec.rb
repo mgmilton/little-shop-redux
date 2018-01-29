df3778c2b5c8fc767780001b6764cc0c2cb2e563
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-29 13:29:48 -0700

    update a merchant features test

--- spec/features/merchant-features/user_can_see_all_merchants_spec.rb
+++ spec/features/merchant-features/user_can_see_all_merchants_spec.rb
@@ -40,18 +40,17 @@ describe "As a user" do
       visit '/merchants'
 
       click_on 'Edit'
-      
+
       expect(current_path).to eq('/merchants/316/edit')
     end
 
     it "I can see and click the delete button to delete a specific merchant and only see remaining merchants" do
       Merchant.create(name: "thing", id: 1)
-      # TODO fix this test, still not working
+
       visit '/merchants'
-      # find('.delete_button_2').click
-      # within(".merchants_#{m2}") do
-        click_on 'delete'
-      # end
+
+      click_on 'delete'
+
       expect(Merchant.count).to eq(0)
     end
   end
