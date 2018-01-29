5b6d7fcd0081af8db7a36413cad990812fe2582e
mgmilton <matt.milton@gmail.com>
2018-01-29 14:53:53 -0700

    renames directory and removes migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127183145) do
+ActiveRecord::Schema.define(version: 20180128233654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20180127183145) do
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "item_count", default: 0
+    t.integer "item_count"
   end
 
 end
