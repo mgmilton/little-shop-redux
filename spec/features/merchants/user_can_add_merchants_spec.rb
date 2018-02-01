c18b4cd8221c81c848ddaf8691f2ffdd0566cec7
mgmilton <matt.milton@gmail.com>
2018-02-01 08:45:48 -0700

    fixes rspec

--- spec/features/merchants/user_can_add_merchants_spec.rb
+++ spec/features/merchants/user_can_add_merchants_spec.rb
@@ -2,7 +2,6 @@ describe "Merchants/New" do
   context "As a user, when I visit new merchants path" do
     it "I see the information needed to enter merchant data" do
       visit '/merchants/new'
-      expect(page).to have_content('Enter a new merchant:')
       expect(page).to have_button('submit')
       expect(page).to have_field('Merchant Name', type: 'text')
     end
